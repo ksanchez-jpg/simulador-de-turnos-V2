4f101f80cfa7af4060ec3729fec594e37af42e13
ksanchez-jpg <ksanchez@ingeniocarmelita.com>
2025-08-28 20:32:20 -0500

    Update streamlit_calculadora_de_dotacion_por_turnos_version_1.py

--- streamlit_calculadora_de_dotacion_por_turnos_version_1.py
+++ streamlit_calculadora_de_dotacion_por_turnos_version_1.py
@@ -3,22 +3,6 @@ import math
 import pandas as pd
 import io
 
-# Establecer la configuración de la página para usar el ancho completo
-st.set_page_config(layout="wide")
-
-# Función para convertir múltiples DataFrames a un archivo Excel en memoria
-def to_excel(dfs_dict):
-    """
-    Convierte un diccionario de DataFrames en un archivo Excel con múltiples hojas.
-    """
-    output = io.BytesIO()
-    # Se usa xlsxwriter ya que es un motor comun, si falla, puede que necesites instalarlo con 'pip install xlsxwriter'
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        for sheet_name, df in dfs_dict.items():
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
-    processed_data = output.getvalue()
-    return processed_data
-
 # Título de la aplicación
 st.title("Calculadora de Personal y Programación de Turnos")
 st.write("Ingrese los parámetros a continuación para calcular el personal necesario y generar la programación de turnos.")
